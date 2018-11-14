5f638ca0b100da67484f747afa8151aa274f35ed
David Munkvold <dmunkvold@gmail.com>
2018-11-14 10:29:47 -0600

    working on dataset generator

--- dataset_builder/dataset_builder.py
+++ dataset_builder/dataset_builder.py
@@ -7,6 +7,18 @@ class DataSetBuilder:
         self.crypto = crypto
         self.hashtags = hashtags
         self.twitter_api = TwitterAPI()
-        self.cryptocompare_api =
+        self.cryptocompare_api = CryptoCompareAPI()
+        self.sample_set = [[],[]]
 
-    def generate_data_set(self, start_date_time, end_date_time)
+    def generate_data_set(self, start_date, num_samples):
+        fetch_tweets(start_date, num_samples)
+        fetch_prices()
+
+    def fetch_tweets(self, start_date, num_samples):
+        for hashtag in self.hashtags:
+            tweets = self.twitter_api.find_tweets_by_hashtag(hashtag, start_date, num_samples)
+            self.sample_set[0] += tweets
+
+    def fetch_prices(self):
+        for tweet in self.sample_set[0]:
+            price = self.cryptocompare_api.get_historical_price(self.crypto, tweet.created_at
--- twitter_api/TwitterAPI.py
+++ twitter_api/TwitterAPI.py
@@ -13,8 +13,8 @@ class TwitterAPI:
         self.auth.set_access_token(self.access_token, self.access_token_secret)
         self.api = tweepy.API(self.auth,wait_on_rate_limit=True)
 
-    def find_tweets_by_hashtag(self, hashtag):
-        for tweet in tweepy.Cursor(self.api.search, q = hashtag,count = 100,
+    def find_tweets_by_hashtag(self, hashtag, start_date, count):
+        for tweet in tweepy.Cursor(self.api.search, q = hashtag,count = count,
                                    lang="en",
-                                   since="2018-04-03").items():
+                                   since=start_date).items():
             print (tweet.created_at, tweet.text)
